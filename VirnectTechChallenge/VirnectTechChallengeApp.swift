fb767ac519b29a1d21b03e7c123bf348b3cd7df1
CoffeeK <pjklord1@daum.net>
2021-11-08 20:28:39 +0900

    initial commit

---
+++ VirnectTechChallenge/VirnectTechChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirnectTechChallengeApp.swift
+//  VirnectTechChallenge
+//
+//  Created by JunKyung.Park on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct VirnectTechChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
